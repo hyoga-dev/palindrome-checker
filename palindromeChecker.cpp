b99fc8a87896a0e44b08809e77528a7d4b441da0
Lamy-lame <75929898+hyoga-dev@users.noreply.github.com>
2024-10-27 17:11:11 +0700

    Add files via upload

---
+++ palindromeChecker.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(const string& txt) {
+	int left = 0;
+	int right = txt.length() - 1;
+	while (right > left) if (txt[left++] != txt[right--]) return false;
+	return true;
+}
+
+bool isPalindrome2(string txt) {
+	string txtRev = txt;
+	reverse(txtRev.begin(), txtRev.end());
+	return (txtRev == txt) ? true : false;
+}
+
+int main() {
+	string kata;
+	cout << "--Check kata--\n\n";
+	cout << "Input kata : "; cin >> kata;
+	cout << "Hasil : " << boolalpha << isPalindrome(kata) << endl;
+}
