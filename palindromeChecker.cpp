8e9edea8ce97d5a986156ba1cbaed04819080cd3
Lamy-lame <75929898+hyoga-dev@users.noreply.github.com>
2024-10-27 18:11:14 +0700

    Update palindromeChecker.cpp

--- palindromeChecker.cpp
+++ palindromeChecker.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
 using namespace std;
 
-bool isPalindrome(const string& txt) {
-	int left = 0;
-	int right = txt.length() - 1;
-	while (right > left) if (txt[left++] != txt[right--]) return false;
-	return true;
-}
+void main() {
+	string txt;
+	cout << "---Check kata---\n\n";
+	cout << "Input kata : "; cin >> txt;
 
-bool isPalindrome2(string txt) {
 	string txtRev = txt;
-	reverse(txtRev.begin(), txtRev.end());
-	return (txtRev == txt) ? true : false;
-}
+	reverse(txtRev.begin(), txtRev.end()); 
 
-int main() {
-	string kata;
-	cout << "--Check kata--\n\n";
-	cout << "Input kata : "; cin >> kata;
-	cout << "Hasil : " << boolalpha << isPalindrome(kata) << endl;
+	cout << "Hasil : " << boolalpha << (txtRev == txt) ? true : false;
 }
